6b9622ff6233ecb1b29972c0ee802b2e9789f0b9
bestdatamaroc-cpu <bestdatamaroc@gmail.com>
2026-08-29 10:55:58 -0500

    Update main.py

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ WICK_THRESHOLD = 0.35
 
 
 def send_telegram_alert(message):
-    url = f"https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage"
+    url = f"https://api.telegram.org/bot{8834699234:AAHnqWUWz8auv0LbJDuMePTaeky8kmqIu0o}/sendMessage"
     payload = {"chat_id": TELEGRAM_CHAT_ID, "text": message, "parse_mode": "Markdown"}
     try:
         requests.post(url, json=payload, timeout=10)
